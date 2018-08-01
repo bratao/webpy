510784e06f7cf3a691d18d356350e08eefe76d59
Bruno Cabral <brataodream@gmail.com>
2018-08-01 16:18:14 -0300

    Missing directory in Python 3.6

--- web/httpserver.py
+++ web/httpserver.py
@@ -194,6 +194,8 @@ class StaticApp(SimpleHTTPRequestHandler):
         self.headers = []
         self.environ = environ
         self.start_response = start_response
+        directory = os.getcwd()
+        self.directory = directory
 
     def send_response(self, status, msg=""):
         #the int(status) call is needed because in Py3 status is an enum.IntEnum and we need the integer behind
